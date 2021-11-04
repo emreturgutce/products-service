cbf3bc6b7251aa09a821132fce9e20fe22ae9dc8
emreturgutce <emreturgutcse@hotmail.com>
2021-11-04 23:38:49 +0300

    eureka client added

---
+++ src/main/java/com/emreturgutce/productsservice/infrastructure/controller/ProductController.java
@@ -0,0 +1,7 @@
+package com.emreturgutce.productsservice.infrastructure.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+}
