47cff5b30cd00eff9649b01dc88a96e9249314ad
emreturgutce <emreturgutcse@hotmail.com>
2021-11-05 11:11:22 +0300

    starting multiple instances of products service

--- src/main/java/com/emreturgutce/productsservice/infrastructure/controller/ProductController.java
+++ src/main/java/com/emreturgutce/productsservice/infrastructure/controller/ProductController.java
@@ -1,7 +1,17 @@
 package com.emreturgutce.productsservice.infrastructure.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("products")
 public class ProductController {
+
+    @GetMapping
+    public ResponseEntity<String> getProducts() {
+        return new ResponseEntity<>("products", HttpStatus.OK);
+    }
 }
